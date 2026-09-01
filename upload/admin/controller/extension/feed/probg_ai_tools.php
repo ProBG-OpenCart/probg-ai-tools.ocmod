5d7edcb611fdfe64c26c89579cc1cd0b5f12e5b4
Veselin Totev <v.totev@probg.com>
2026-09-01 13:50:14 +0300

    fix: harden AI Tools admin settings

--- upload/admin/controller/extension/feed/probg_ai_tools.php
+++ upload/admin/controller/extension/feed/probg_ai_tools.php
@@ -4,103 +4,96 @@ class ControllerExtensionFeedProbgAiTools extends Controller {
 
   public function index() {
     $this->load->language('extension/feed/probg_ai_tools');
-
     $this->document->setTitle($this->language->get('heading_title'));
-
     $this->load->model('setting/setting');
 
     if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
       $this->model_setting_setting->editSetting('feed_probg_ai_tools', $this->request->post);
-
       $this->session->data['success'] = $this->language->get('text_success');
 
       $this->response->redirect($this->url->link(
-        'marketplace/extension',
-        'user_token=' . $this->session->data['user_token'] . '&type=feed',
+        'extension/feed/probg_ai_tools',
+        'user_token=' . $this->session->data['user_token'],
         true
       ));
     }
 
-    $data['heading_title'] = $this->language->get('heading_title');
+    $language_keys = array(
+      'heading_title',
+      'text_edit',
+      'text_enabled',
+      'text_disabled',
+      'text_yes',
+      'text_no',
+      'text_ai_catalog_url',
+      'entry_status',
+      'entry_products',
+      'entry_categories',
+      'entry_brands',
+      'entry_product_limit',
+      'entry_ai_sitemap_url',
+      'entry_llms_txt',
+      'entry_llms_json',
+      'entry_llms_full',
+      'entry_products_graph',
+      'entry_ai_catalog',
+      'entry_ai_policy',
+      'entry_search_index',
+      'entry_semantic_graph',
+      'entry_htaccess',
+      'entry_site_description',
+      'help_status',
+      'help_products',
+      'help_categories',
+      'help_brands',
+      'help_product_limit',
+      'help_htaccess',
+      'help_site_description',
+      'help_ai_policy',
+      'help_ai_sitemap',
+      'help_ai_catalog',
+      'help_search_index',
+      'help_semantic_graph',
+      'help_products_graph',
+      'help_llms_txt',
+      'help_llms_json',
+      'help_llms_full',
+      'text_htaccess_rule',
+      'tab_general',
+      'tab_ai_sitemap',
+      'tab_ai_catalog',
+      'tab_llms',
+      'tab_products_graph',
+      'tab_advanced',
+      'button_save',
+      'button_cancel'
+    );
 
-    $data['text_edit'] = $this->language->get('text_edit');
-    $data['text_enabled'] = $this->language->get('text_enabled');
-    $data['text_disabled'] = $this->language->get('text_disabled');
-    $data['text_yes'] = $this->language->get('text_yes');
-    $data['text_no'] = $this->language->get('text_no');
-
-    $data['entry_status'] = $this->language->get('entry_status');
-    $data['entry_products'] = $this->language->get('entry_products');
-    $data['entry_categories'] = $this->language->get('entry_categories');
-    $data['entry_brands'] = $this->language->get('entry_brands');
-    $data['entry_product_limit'] = $this->language->get('entry_product_limit');
-    $data['entry_ai_sitemap_url'] = $this->language->get('entry_ai_sitemap_url');
-    $data['entry_llms_txt'] = $this->language->get('entry_llms_txt');
-    $data['entry_llms_json'] = $this->language->get('entry_llms_json');
-    $data['entry_llms_full'] = $this->language->get('entry_llms_full');
-    $data['entry_products_graph'] = $this->language->get('entry_products_graph');
-
-    $data['help_status'] = $this->language->get('help_status');
-    $data['help_products'] = $this->language->get('help_products');
-    $data['help_categories'] = $this->language->get('help_categories');
-    $data['help_brands'] = $this->language->get('help_brands');
-    $data['help_product_limit'] = $this->language->get('help_product_limit');
-    $data['help_htaccess'] = $this->language->get('help_htaccess');
-
-    $data['text_htaccess_rule'] = $this->language->get('text_htaccess_rule');
-
-    $data['tab_general'] = $this->language->get('tab_general');
-    $data['tab_ai_sitemap'] = $this->language->get('tab_ai_sitemap');
-    $data['tab_llms'] = $this->language->get('tab_llms');
-    $data['tab_products_graph'] = $this->language->get('tab_products_graph');
-    $data['tab_advanced'] = $this->language->get('tab_advanced');
-
-    $data['entry_ai_sitemap'] = $this->language->get('entry_ai_sitemap');
-    $data['entry_urls'] = $this->language->get('entry_urls');
-    $data['entry_htaccess'] = $this->language->get('entry_htaccess');
-    $data['entry_site_description'] = $this->language->get('entry_site_description');
-
-    $data['help_site_description'] = $this->language->get('help_site_description');
-    $data['entry_ai_policy'] = $this->language->get('entry_ai_policy');
-    $data['help_ai_policy'] = $this->language->get('help_ai_policy');
-    $data['entry_ai_sitemap'] = $this->language->get('entry_ai_sitemap');
-    $data['help_ai_sitemap'] = $this->language->get('help_ai_sitemap');
-    $data['entry_search_index'] = $this->language->get('entry_search_index');
-    $data['help_search_index'] = $this->language->get('help_search_index');
-
-
-    $data['button_save'] = $this->language->get('button_save');
-    $data['button_cancel'] = $this->language->get('button_cancel');
+    foreach ($language_keys as $key) {
+      $data[$key] = $this->language->get($key);
+    }
 
     $data['error_warning'] = isset($this->error['warning']) ? $this->error['warning'] : '';
+    $data['error_product_limit'] = isset($this->error['product_limit']) ? $this->error['product_limit'] : '';
+
+    $data['success'] = '';
+    if (isset($this->session->data['success'])) {
+      $data['success'] = $this->session->data['success'];
+      unset($this->session->data['success']);
+    }
 
     $data['breadcrumbs'] = array();
-
     $data['breadcrumbs'][] = array(
       'text' => $this->language->get('text_home'),
-      'href' => $this->url->link(
-        'common/dashboard',
-        'user_token=' . $this->session->data['user_token'],
-        true
-      )
+      'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
     );
-
     $data['breadcrumbs'][] = array(
       'text' => $this->language->get('text_extension'),
-      'href' => $this->url->link(
-        'marketplace/extension',
-        'user_token=' . $this->session->data['user_token'] . '&type=feed',
-        true
-      )
+      'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=feed', true)
     );
-
     $data['breadcrumbs'][] = array(
       'text' => $this->language->get('heading_title'),
-      'href' => $this->url->link(
-        'extension/feed/probg_ai_tools',
-        'user_token=' . $this->session->data['user_token'],
-        true
-      )
+      'href' => $this->url->link('extension/feed/probg_ai_tools', 'user_token=' . $this->session->data['user_token'], true)
     );
 
     $data['action'] = $this->url->link(
@@ -145,22 +138,22 @@ class ControllerExtensionFeedProbgAiTools extends Controller {
       $data['feed_probg_ai_tools_product_limit'] = 0;
     }
 
-    $data['ai_sitemap_url'] = HTTPS_CATALOG . 'ai-sitemap.xml';
-    $data['llms_txt_url'] = HTTPS_CATALOG . 'llms.txt';
-    $data['llms_json_url'] = HTTPS_CATALOG . 'llms.json';
-    $data['llms_full_url'] = HTTPS_CATALOG . 'llms-full.txt';
-    $data['products_graph_url'] = HTTPS_CATALOG . 'products.graph.json';
-    $data['ai_catalog_url'] = HTTPS_CATALOG . 'ai-catalog.json';
-    $data['search_index_url'] = HTTPS_CATALOG . 'search-index.json';
-    $data['semantic_graph_url'] = HTTPS_CATALOG . 'semantic.graph.json';
+    $catalog_url = rtrim(HTTPS_CATALOG, '/') . '/';
+    $data['ai_sitemap_url'] = $catalog_url . 'ai-sitemap.xml';
+    $data['llms_txt_url'] = $catalog_url . 'llms.txt';
+    $data['llms_json_url'] = $catalog_url . 'llms.json';
+    $data['llms_full_url'] = $catalog_url . 'llms-full.txt';
+    $data['products_graph_url'] = $catalog_url . 'products.graph.json';
+    $data['ai_catalog_url'] = $catalog_url . 'ai-catalog.json';
+    $data['ai_policy_url'] = $catalog_url . 'ai-policy.txt';
+    $data['search_index_url'] = $catalog_url . 'search-index.json';
+    $data['semantic_graph_url'] = $catalog_url . 'semantic.graph.json';
 
     $data['header'] = $this->load->controller('common/header');
     $data['column_left'] = $this->load->controller('common/column_left');
     $data['footer'] = $this->load->controller('common/footer');
 
-    $this->response->setOutput(
-      $this->load->view('extension/feed/probg_ai_tools', $data)
-    );
+    $this->response->setOutput($this->load->view('extension/feed/probg_ai_tools', $data));
   }
 
   public function install() {
@@ -187,13 +180,20 @@ class ControllerExtensionFeedProbgAiTools extends Controller {
 
   public function uninstall() {
     $this->load->model('setting/setting');
-
     $this->model_setting_setting->deleteSetting('feed_probg_ai_tools');
   }
 
   protected function validate() {
     if (!$this->user->hasPermission('modify', 'extension/feed/probg_ai_tools')) {
       $this->error['warning'] = $this->language->get('error_permission');
+    }
+
+    if (isset($this->request->post['feed_probg_ai_tools_product_limit'])) {
+      $limit = trim((string)$this->request->post['feed_probg_ai_tools_product_limit']);
+
+      if ($limit === '' || !ctype_digit($limit)) {
+        $this->error['product_limit'] = $this->language->get('error_product_limit');
+      }
     }
 
     return !$this->error;
